b57af617ed6b8ba7992194aa65170274361df219
Yuliya Lopatina <yu.ol.lopatina@gmail.com>
2024-08-12 20:18:07 +0200

    Created xcode project

---
+++ HealthTracker/HealthTracker/HealthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerApp.swift
+//  HealthTracker
+//
+//  Created by Yuliya Lapatsina on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
