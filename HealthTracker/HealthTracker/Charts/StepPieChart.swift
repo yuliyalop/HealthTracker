d1fd4ca44ec982ad9a2af2834027f692e47f90ef
Yuliya Lopatina <yu.ol.lopatina@gmail.com>
2024-09-15 19:45:21 +0200

    Initial StepPieChart

---
+++ HealthTracker/HealthTracker/Charts/StepPieChart.swift
@@ -0,0 +1,52 @@
+//
+//  StepPieChart.swift
+//  HealthTracker
+//
+//  Created by Yuliya Lapatsina on 15/09/2024.
+//
+
+import SwiftUI
+import Charts
+
+struct StepPieChart: View {
+    var chartData: [WeekdayChartData] = []
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Label("Averages", systemImage: "calendar")
+                        .font(.title3.bold())
+                        .foregroundStyle(.pink)
+                    Text("Last 28 days")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .foregroundStyle(.secondary)
+            }
+            .padding(.bottom, 12)
+            
+            Chart {
+                ForEach(chartData) { weekday in
+                    SectorMark(angle: .value("Average Steps", weekday.value),
+                               innerRadius: .ratio(0.618),
+                               angularInset: 1)
+                    .foregroundStyle(.pink.gradient)
+                    .cornerRadius(6)
+                }
+            }
+            .chartLegend(.hidden)
+            .frame(height: 240)
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+    }
+}
+
+#Preview {
+    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: HealthMetric.mockData))
+}
--- HealthTracker/HealthTracker/Screens/DashboardView.swift
+++ HealthTracker/HealthTracker/Screens/DashboardView.swift
@@ -47,30 +47,8 @@ struct DashboardView: View {
                     
                     StepBarChart(selectedStat: selectedStat, chartData: hkManager.stepsData )
                     
-                    VStack {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Averages", systemImage: "calendar")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
-                                Text("Last 28 days")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
-                            }
-                            
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
-                                .foregroundStyle(.secondary)
-                        }
-                        .padding(.bottom, 12)
-                        
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.secondary)
-                            .frame(height: 240)
-                    }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: hkManager.stepsData))
+
                 }
             }
             .padding()
