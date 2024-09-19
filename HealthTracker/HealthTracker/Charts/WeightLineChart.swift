35981ab010669cca3d6ff796d0322120022dbd2d
Yuliya Lopatina <yu.ol.lopatina@gmail.com>
2024-09-19 19:17:53 +0200

    WeightLineChart styling

--- HealthTracker/HealthTracker/Charts/WeightLineChart.swift
+++ HealthTracker/HealthTracker/Charts/WeightLineChart.swift
@@ -12,11 +12,9 @@ struct WeightLineChart: View {
     var selectedStat: HealthMetricContext = .weight
     var chartData: [HealthMetric]
     
-//    var avgWeightCount: Double {
-//        guard !chartData.isEmpty else { return 0 }
-//        let totalSteps = chartData.reduce(0) { $0 + $1.value }
-//        return totalSteps/Double(chartData.count)
-//    }
+    var minValue: Double {
+        chartData.map{ $0.value}.min() ?? 0
+    }
     
     var body: some View {
         VStack {
@@ -39,8 +37,37 @@ struct WeightLineChart: View {
             .foregroundStyle(.secondary)
             
             Chart {
+                RuleMark(y: .value("Goal", 155))
+                    .foregroundStyle(.mint)
+                    .lineStyle(.init(lineWidth: 1, dash: [5]))
+                
                 ForEach(chartData) { weight in
+                    AreaMark(x: .value("Day", weight.date, unit: .day),
+                             yStart: .value("Value", weight.value),
+                             yEnd: .value("MinValue", minValue))
+                    .foregroundStyle(Gradient(colors: [.indigo.opacity(0.5), .clear]))
+                    .interpolationMethod(.catmullRom)
+                    
                     LineMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
+                        .foregroundStyle(.indigo)
+                        .interpolationMethod(.catmullRom)
+                        .symbol(.circle)
+                }
+                
+                
+            }
+            .frame(height: 150)
+            .chartYScale(domain: .automatic(includesZero: false))
+            .chartXAxis {
+                AxisMarks {
+                    AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+                }
+            }
+            .chartYAxis {
+                AxisMarks { value in
+                    AxisGridLine()
+                        .foregroundStyle(Color.secondary.opacity(0.3))
+                    AxisValueLabel()
                 }
             }
         }
